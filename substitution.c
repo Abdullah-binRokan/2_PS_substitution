1ae39b8d7d58026bb52fef61d2338a955dc4f2a3
AR <aarokan@gmail.com>
2024-07-25 19:43:29 +0300

    write psuedocode

--- substitution.c
+++ substitution.c
@@ -3,5 +3,30 @@
 
 int main(void)
 {
-    
+    // get key
+
+    // validate key
+        // if (key != 26 char)
+            // print corresponding error message and return 1 
+        // if (key has non-alphabetic char)
+            // print corresponding error message and return 1 
+        // if (key has repeated char)
+            // print corresponding error message and return 1 
+
+    // get plaintext
+        // output plaintext: then prompt for a string input
+
+    // encipher
+        // define ciphertext array
+
+        // for each plaintext letter
+            // if (letter = non-alphabetic)
+                // leave non-alphabetic char as is
+                // push it to ciphertext array
+            // else
+                // preserve case
+                // determine what letter it maps to
+                // push it to ciphertext array
+
+    // print ciphertext
 }
